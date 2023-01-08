ce0a332b4b53a47252d9592bd3e5abcca8cfaca8
Documenter.jl <documenter@juliadocs.github.io>
2023-01-08 22:19:51 +0000

    build based on 7425b12

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DeepUnfoldedCDLMotif","category":"page"},{"location":"#DeepUnfoldedCDLMotif","page":"Home","title":"DeepUnfoldedCDLMotif","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DeepUnfoldedCDLMotif.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DeepUnfoldedCDLMotif]","category":"page"}]
+}
